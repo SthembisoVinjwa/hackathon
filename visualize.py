f7eae0e38034f98668348ea8c43647cdad174fef
Vinjwa, S, Mr [23745444@sun.ac.za] <23745444@sun.ac.za>
2023-08-16 08:35:40 +0200

    Better visualisation

--- visualize.py
+++ visualize.py
@@ -7,15 +7,20 @@ claims_path = 'claims_data.csv'
 try:
     with open(claims_path, "r") as file:
         lines = file.readlines()
-        for line in lines[1:2]:
-            vehicle_id = line.split(',')[0]
+        vehicle_id = ['87055861', '150986252'] # Insert list Vehicle ID to generate charts for
+        for id in vehicle_id: 
+            vehicle_data_path = 'telematics_data/final/' + id + '.csv'
+            
+            df = pd.read_csv(vehicle_data_path)
+            df['timestamp'] = pd.to_datetime(df['timestamp'])
+            
+            desired_day = '2022-02-15' # Select any day you want within the dataset dates
+            single_day_data = df[df['timestamp'].dt.date == pd.to_datetime(desired_day).date()]
 
-            vehicle_id = '90293012'
-
-            vehicle_data_path = 'telematics_data/final/' + vehicle_id + '.csv'
             try:
-                vehicle_data = pd.read_csv(vehicle_data_path)
-                fig = px.scatter(vehicle_data, x='coordinate_latitude', y='coordinate_latitude', title='Timestamp vs. Speed Vehicle ID ' + vehicle_id)
+                fig = px.line(single_day_data, x='timestamp', y='speed', 
+                                title=f'Speed on {desired_day} for Vehicle {id}', 
+                                text='heading', line_shape='linear', color='SP_NAME')
                 fig.show()
             except FileNotFoundError:
                 print("Vehicle data file not found")
